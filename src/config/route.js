ca1ea9385f2e48d7f3537f16ce30e2f39ae79ca1
Dubravko <dev.richard.npm98@gmail.com>
2023-12-03 22:33:24 +0700

    udpate code

--- src/config/route.js
+++ src/config/route.js
@@ -1,11 +1,13 @@
-// const SERVER_API_URL = '';
-// const NEXT_PUBLIC_API_URL = '';
+const NEXT_PUBLIC_SERVER_API = 'ws://supernova-api.demo-website.click:8083';
+const NEXT_PUBLIC_SERVER_API_ENDPOINT = '';
 
 module.exports = {
 	rewrites: [
-		// {
-		// 	source: `${process.env.NEXT_PUBLIC_API_URL || NEXT_PUBLIC_API_URL}/:path*`,
-		// 	destination: `${process.env.SERVER_API_URL || SERVER_API_URL}${process.env.NEXT_PUBLIC_API_URL || NEXT_PUBLIC_API_URL}`,
-		// },
+		{
+			source: `${process.env.NEXT_PUBLIC_SERVER_API_ENDPOINT || NEXT_PUBLIC_SERVER_API_ENDPOINT}/:path*`,
+			destination: `${process.env.NEXT_PUBLIC_SERVER_API || NEXT_PUBLIC_SERVER_API}${
+				process.env.NEXT_PUBLIC_SERVER_API_ENDPOINT || NEXT_PUBLIC_SERVER_API_ENDPOINT
+			}/:path*`,
+		},
 	],
 };
